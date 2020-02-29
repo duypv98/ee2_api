5a2bc1abc6f7a93b91658333ae14cd743f46eb89
Duy Phung <duypv98@gmail.com>
2020-02-29 23:53:39 +0700

    refactor: auth and user routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,21 @@
 const asyncRoute = require('utils/asyncRoute');
-const authCore = require('core/auth.core');
+const User = require('db/models/User');
+const { InvalidUsernameOrEmailError, InvalidPasswordError } = require('common/error');
+const encryption = require('utils/encryption');
+const jsonWebToken = require('utils/jsonWebToken');
 
 const login = asyncRoute(async (req, res) => {
   const { username, password } = req.body;
-  const dataResponse = await authCore.checkLogin(username, password);
-  res.json({
-    data: dataResponse,
-  });
+  const user = await User.findOne().or([
+    { username }, { email: username }
+  ]);
+  if (!user) throw new InvalidUsernameOrEmailError();
+  if (encryption.isEqual(password, user.password_hashed)) {
+    const userId = user.get('_id');
+    const token = jsonWebToken.signToken({ uid: userId });
+    res.json({ data: { userId, token } });
+  }
+  throw new InvalidPasswordError();
 });
 
 module.exports = { login };
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,33 +1,76 @@
+const configs = require('configs/index');
 const asyncRoute = require('utils/asyncRoute');
-const userCore = require('core/users.core');
+const User = require('db/models/User');
 const constant = require('common/constant');
+const {
+  UserNotFoundError,
+  TakenUsernameError,
+  TakenEmailError,
+  InvalidEmailFormatError,
+} = require('common/error');
+const { VerifyUserEmail } = require('common/mail');
+const validation = require('utils/validation');
+const encryption = require('utils/encryption');
+const jsonWebToken = require('utils/jsonWebToken');
 
-const { UserAction } = constant;
+const { UserAction, SelectField } = constant;
 
 const getUserById = asyncRoute(async (req, res) => {
-  const dataResponse = await userCore.findUserById(req.uid);
-  res.json({
-    data: dataResponse,
-  });
+  const data = await User.findOne(
+    { _id: req.uid },
+    { password_hashed: SelectField.NO }
+  );
+  if (!data) throw new UserNotFoundError();
+  res.json({ data });
 });
 
 const createNewUser = asyncRoute(async (req, res) => {
-  const dataResponse = await userCore.saveNewUser(req.body);
+  const {
+    username, email, password, full_name, phone_number,
+  } = req.body;
+  const exUserWithUsername = await User.findOne({ username });
+  if (exUserWithUsername) throw new TakenUsernameError();
+  const exUserWithEMail = await User.findOne({ email });
+  if (exUserWithEMail) throw new TakenEmailError();
+  if (!validation.isValidEmail(email)) throw new InvalidEmailFormatError();
+
+  const [newUser] = await User.create([{
+    username,
+    password_hashed: encryption.encrypt(password),
+    email,
+    email_verified: false,
+    phone_number,
+    full_name,
+    account_type: 'normal'
+  }]);
+  const verifyLink = `${configs.FE_URL}/verify?userId=${newUser.id}`;
+  const verifyEmail = new VerifyUserEmail({
+    to: `${newUser.email}`,
+    html: `Xin chào ${newUser.full_name}, <br/>
+    Bạn vừa mới đăng ký tạo tài khoản mới trên Easy-Event <br/>
+    Chúng tôi cần bạn xác nhận email đăng ký, vui lòng ấn vào đường dẫn sau để hoàn tất: <br/>
+    <a href="${verifyLink}">${verifyLink}</a><br/>
+    Chúc bạn có những trải nghiệm tuyệt với cùng Easy-Event`
+  });
+
+  verifyEmail.send();
+
   res.json({
-    data: dataResponse,
+    data: {
+      userId: newUser.id,
+      token: jsonWebToken.signToken({ uid: newUser.id })
+    },
   });
 });
 
 const updateUser = asyncRoute(async (req, res) => {
   const { action } = req.body;
   const { userId } = req.params;
-  let dataResponse = {};
+  let updatedUser;
   if (action === UserAction.VERIFY) {
-    dataResponse = await userCore.updateVerifyEmail(userId);
+    updatedUser = await User.findByIdAndUpdate(userId, { email_verified: true }, { new: true });
   }
-  res.json({
-    data: dataResponse,
-  });
+  return res.json({ data: { updatedUser } });
 });
 
 module.exports = {
